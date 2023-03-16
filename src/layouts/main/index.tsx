19a7252c28b0929ba65824d74660afde65b2aa0f
Yehor <kravtsovyehor@gmail.com>
2023-03-16 21:27:09 +0200

    Cleaned dependencies

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 
 import { Loader } from "components/ui";
 
@@ -14,17 +13,5 @@ export const MainLayout = () => (
         </Suspense>
       </main>
     </div>
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable={false}
-      pauseOnHover
-      theme="light"
-    />
   </>
 );
